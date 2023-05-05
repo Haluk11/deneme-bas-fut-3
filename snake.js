3e1397e2d7856a2192fc30d993e507f57c96b8f8
Haluk11 <halukesad9@hotmail.com>
2023-05-05 14:29:24 +0300

    snake.js
    update on snake

--- snake.js
+++ snake.js
@@ -8,9 +8,19 @@ var context;
 var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
 
+//snake go speed
+
+var velocityX = 0;
+var velocityY = 0;
+
+//new body parts
+var snakeBody = [];
+
 //food
 var foodX;
 var foodY;
+
+var gameOver = false;
 
 window.onload = function(){
     board = document.getElementById("board");
@@ -20,26 +30,77 @@ window.onload = function(){
 
     foodRandom();
     document.addEventListener("keyup", changeDirection);
-    update();
+    //update();
+    setInterval(update,100) //we call update func every 0.1 sec
 }
-function update(){
+function update()
+{
+    if(gameOver){
+        return;
+    }
+
     //grid
     context.fillStyle="black";
     context.fillRect(0, 0, board.width, board.height);
 
-    //snake 
-    context.fillStyle="lime";
-    context.fillRect (snakeX,snakeY,blockSize,blockSize);
-
     //food
     context.fillStyle="red";
     context.fillRect(foodX,foodY,blockSize,blockSize);
+
+    //eat food and relocate
+    if(snakeX == foodX && snakeY == foodY){
+        snakeBody.push([foodX, foodY])
+        foodRandom();
+    }
+    //en sondan başlayıp bütün parçaların followlaması
+    for(let i = snakeBody.length-1; i>0;i--){
+        snakeBody[i] = snakeBody[i-1];
+    }
+    if (snakeBody.length){
+        snakeBody[0] = [snakeX,snakeY];
+    }
+
+    //snake 
+    context.fillStyle="lime";
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect (snakeX,snakeY,blockSize,blockSize);
+    for(let i=0; i < snakeBody.length; i++){
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+    //game over condition
+    if(snakeX < 0 || snakeX > cols*blockSize || snakeY > rows*blockSize) {
+        gameOver = true;
+        alert("Game Over");
+    } 
+
+    for (let i = 0; i < snakeBody.length; i++){
+        if(snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]){
+            gameOver = true;
+            alert("Game Over");
+        }
+    }
 }
 /// randomize food spawn location
 function foodRandom(){
     foodX = Math.floor(Math.random() * cols) * blockSize;
     foodY = Math.floor(Math.random() * cols) * blockSize;
 }
-function changeDirection(){
-    
+function changeDirection(e){
+    if(e.code == "ArrowUp" && velocityY !=1){
+        velocityX = 0;
+        velocityY = -1;
+    }
+    if(e.code == "ArrowDown" && velocityY !=-1){
+        velocityX = 0;
+        velocityY = 1;
+    }
+    if(e.code == "ArrowLeft" && velocityX !=-1){
+        velocityX = -1;
+        velocityY = 0;
+    }
+    if(e.code == "ArrowRight" && velocityX !=-1){
+        velocityX = 1;
+        velocityY = 0;
+    }
 }
